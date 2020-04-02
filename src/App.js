c4ec251dbc0f1c342a8ecd6ab137897df1321510
mati <mateuszgrabiec7@gmail.com>
2020-04-03 01:02:41 +0200

    login form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {Button,Form, FormGroup, Label, Input}
+from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="login-form">
+      <h1><span className="font-weight-bold">Worker-website</span>.com</h1>
+      <h2 className="text-center">Welcome</h2>
+      <FormGroup>
+        <Label>
+          Email
+        </Label>
+        <Input type="email" placeholder="Email"></Input>
+      </FormGroup>
+      <FormGroup>
+        <Label>
+          Password
+        </Label>
+        <Input type="password" placeholder="Password"></Input>
+      </FormGroup>
+      <Button className="btn btn-success">Log in</Button>
+      <div className="text-center">
+        <a href="/sign-up">Sign up</a>
+        <span className="p-2">|</span>
+        <a href="/remebr-passsword">Forgot Password</a>
+      </div>
+    </Form>
   );
 }
 
